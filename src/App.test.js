b7f7188e108647a6b3c67ba101c0c75ec85394bc
Patrick Chin <pchin@nvidia.com>
2020-01-13 14:01:12 +0800

    changed app text

--- src/App.test.js
+++ src/App.test.js
@@ -4,12 +4,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/I was changed on the Feature branch/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test('renders learn react link 2', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/I was changed on the Feature branch/i);
   expect(linkElement).toBeInTheDocument();
 });
